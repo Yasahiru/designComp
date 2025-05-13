4f37f66e14384930ece7cea6d87ed8f7b12971fe
yasahiru <legendem718@gmail.com>
2025-05-13 19:54:55 +0200

    first code

--- app/src/main/java/com/cmc/compdesign/presentation/activity/RegistrationActivity.kt
+++ app/src/main/java/com/cmc/compdesign/presentation/activity/RegistrationActivity.kt
@@ -1,4 +1,13 @@
 package com.cmc.compdesign.presentation.activity
 
-class RegistrationActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cmc.compdesign.R
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
 }
